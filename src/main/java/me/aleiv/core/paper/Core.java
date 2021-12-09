74d6eb5d27ef305f89e627b8e1582871c2426d0d
AleIV <alejandrocorrales0201@gmail.com>
2021-12-08 22:17:00 -0600

    SkinTool

--- src/main/java/me/aleiv/core/paper/Core.java
+++ src/main/java/me/aleiv/core/paper/Core.java
@@ -1,21 +1,17 @@
 package me.aleiv.core.paper;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import co.aikar.commands.PaperCommandManager;
 import kr.entree.spigradle.annotations.SpigotPlugin;
 import lombok.Getter;
-import me.aleiv.core.paper.commands.GlobalCMD;
-import me.aleiv.core.paper.listeners.GlobalListener;
+import me.aleiv.core.paper.skins.SkinCMD;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import us.jcedeno.libs.rapidinv.RapidInvManager;
 
 @SpigotPlugin
 public class Core extends JavaPlugin {
 
     private static @Getter Core instance;
-    private @Getter Game game;
     private @Getter PaperCommandManager commandManager;
     private @Getter static MiniMessage miniMessage = MiniMessage.get();
 
@@ -23,24 +19,9 @@ public class Core extends JavaPlugin {
     public void onEnable() {
         instance = this;
 
-        RapidInvManager.register(this);
-        BukkitTCT.registerPlugin(this);
-        NegativeSpaces.registerCodes();
-
-        game = new Game(this);
-        game.runTaskTimerAsynchronously(this, 0L, 20L);
-
-        RapidInvManager.register(this);
-
-        //LISTENERS
-
-        Bukkit.getPluginManager().registerEvents(new GlobalListener(this), this);
-
         //COMMANDS
-        
         commandManager = new PaperCommandManager(this);
-
-        commandManager.registerCommand(new GlobalCMD(this));
+        commandManager.registerCommand(new SkinCMD(this));
 
     }
 
